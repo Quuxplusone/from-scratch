59e8c13e82314cad5add5c0691635e67aa6ab544
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-05-14 22:07:08 -0700

    Replace the dumb std::mutex with Ulrich Drepper's first implementation.
    See Ulrich Drepper's paper "Futexes are Tricky" (2011) here:
    https://www.akkadia.org/drepper/futex.pdf
    
    This is the implementation from page 7. It avoids making any system calls
    at all, in the "fast path" case that the mutex is uncontended.

--- include/scratch/bits/concurrency/mutex.h
+++ include/scratch/bits/concurrency/mutex.h
@@ -9,26 +9,44 @@ namespace scratch {
 class mutex
 {
     static constexpr int UNLOCKED = 0;
-    static constexpr int LOCKED = 1;
+    static constexpr int LOCKED_WITHOUT_WAITERS = 1;
+    static constexpr int LOCKED_WITH_WAITERS = 2;
     std::atomic<int> m_futex;
 public:
     constexpr mutex() noexcept : m_futex(UNLOCKED) {}
 
     bool try_lock() {
-        return (m_futex.exchange(LOCKED) == UNLOCKED);
+        int x = UNLOCKED;
+        if (m_futex.compare_exchange_weak(x, LOCKED_WITHOUT_WAITERS)) {
+            // the uncontended case
+            return true;
+        } else {
+            return false;
+        }
     }
 
     void lock() {
-        int old_value = m_futex.exchange(LOCKED);
-        while (old_value != UNLOCKED) {
-            futex_wait(&m_futex, LOCKED);
-            old_value = m_futex.exchange(LOCKED);
+        while (true) {
+            int x = UNLOCKED;
+            if (m_futex.compare_exchange_weak(x, LOCKED_WITHOUT_WAITERS)) {
+                // the uncontended case
+                break;
+            } else if (x == LOCKED_WITH_WAITERS) {
+                futex_wait(&m_futex, LOCKED_WITH_WAITERS);
+            } else if (m_futex.compare_exchange_weak(x, LOCKED_WITH_WAITERS)) {
+                futex_wait(&m_futex, LOCKED_WITH_WAITERS);
+            }
         }
     }
 
     void unlock() {
-        m_futex = UNLOCKED;
-        futex_wake_one(&m_futex);
+        int locked_state = m_futex--;
+        if (locked_state == LOCKED_WITHOUT_WAITERS) {
+            // the uncontended case
+        } else {
+            m_futex = UNLOCKED;
+            futex_wake_one(&m_futex);
+        }
     }
 };
 
