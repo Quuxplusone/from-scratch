237ef5d8e533fcb3371fbaa2f022426d62cecc35
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-05-14 19:33:49 -0700

    Add an implementation of std::unique_lock.

--- include/scratch/bits/concurrency/tag-types.h
+++ include/scratch/bits/concurrency/tag-types.h
@@ -3,7 +3,11 @@
 namespace scratch {
 
 struct adopt_lock_t { explicit adopt_lock_t() = default; };
+struct defer_lock_t { explicit defer_lock_t() = default; };
+struct try_to_lock_t { explicit try_to_lock_t() = default; };
 
 inline constexpr adopt_lock_t adopt_lock{};
+inline constexpr defer_lock_t defer_lock{};
+inline constexpr try_to_lock_t try_to_lock{};
 
 } // namespace scratch
