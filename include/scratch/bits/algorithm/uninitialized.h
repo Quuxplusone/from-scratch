67c77828ca61cf1ecd4427013131addd5e73dd2a
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-06-02 22:31:22 -0700

    Reimplement the "uninitialized memory" algorithms in terms of allocators.
    This way, they can be used to implement allocator-aware containers.
    
    The standard `allocator_traits` provides static member functions
    `construct` and `destroy`, but does not provide `default_construct`.
    This is surprising at first glance, because it seems like
    default-initialization is what you'd want to use for e.g. `vector::resize()`.
    
    However, if `vector v(n)` default-initialized its elements by default,
    we'd have two problems:
    
    - Value-initialized elements of primitive type (e.g. `int`) are
      guaranteed to be initialized to `0`, which is in some sense "safer"
      and perhaps also more useful. So `resize` and the constructor are
      specified by the Standard to value-initialize rather than
      default-initialize.
    
    - It would complicate the job of `scoped_allocator_adaptor`.
      The adaptor's job is basically to take the arguments it's passed
      and forward them on to the outer allocator's `construct`, with
      one more argument (an instance of the inner allocator) tacked on
      the end. So `scoped_allocator_adaptor` is forbidden to
      "default-construct" any objects of allocator-aware types, even
      if the caller were to ask for it.
    
    So the standard `vector::resize` worms its way around this difficulty
    by explicitly stating that the new values inserted by a `resize` are
    "default-inserted", which is different from "default-constructed":
    
    > The type `T` is DefaultInsertable [...] if the following expression is
    > well-formed:
    >
    >     std::allocator_traits<A>::construct(m, p);
    >
    > By default, this will call placement-new, as by `::new((void*)p) T()`
    > (that is, value-initialize the object pointed to by p).
    > If value-initialization is undesirable, for example, if the object is
    > of non-class type and zeroing out is not needed, it can be avoided by
    > providing a custom `Allocator::construct`.
    
    http://en.cppreference.com/w/cpp/container/vector/resize
    http://en.cppreference.com/w/cpp/concept/DefaultInsertable
    
    The idea of providing a custom allocator type just to default-initialize
    `int` objects seems crazy, but you could certainly do that if you really
    wanted to.

--- include/scratch/bits/algorithm/uninitialized.h
+++ include/scratch/bits/algorithm/uninitialized.h
@@ -1,46 +1,8 @@
 #pragma once
 
+#include "scratch/bits/algorithm/allocator-uninitialized.h"
+#include "scratch/bits/containers/allocator.h"
 #include "scratch/bits/traits-classes/iterator-traits.h"
-#include "scratch/bits/algorithm/destroy.h"
-#include "scratch/bits/algorithm/move-iterator.h"
-
-#include <new>
-
-namespace scratch::detail {
-
-template<class FwdIt, class F>
-void uninitialized_construct_via(FwdIt first, FwdIt last, const F& construct)
-{
-    FwdIt current = first;
-    try {
-        while (current != last) {
-            construct(&*current);  // Construct an object at "current".
-            ++current;
-        }
-    } catch (...) {
-        scratch::destroy(first, current);
-        throw;
-    }
-}
-
-template<class FwdIt, class Size, class F>
-void uninitialized_construct_n_via(FwdIt first, Size count, const F& construct)
-{
-    FwdIt current = first;
-    try {
-        while (count > 0) {
-            construct(&*current);  // Construct an object at "current".
-            ++current;
-            --count;
-        }
-    } catch (...) {
-        scratch::destroy(first, current);
-        throw;
-    }
-    return current;
-}
-
-} // namespace scratch::detail
 
 namespace scratch {
 
@@ -48,124 +10,96 @@ template<class It, class FwdIt>
 FwdIt uninitialized_copy(It first, It last, FwdIt dest)
 {
     using T = iterator_value_type_t<FwdIt>;
-    FwdIt current = dest;
-    try {
-        while (first != last) {
-            ::new (&*current) T(*first);
-            ++first;
-            ++current;
-        }
-    } catch (...) {
-        scratch::destroy(dest, current);
-        throw;
-    }
-    return current;
+    scratch::allocator<T> a;
+    return uninitialized_copy(first, last, dest, a);
 }
 
 template<class It, class FwdIt>
 FwdIt uninitialized_move(It first, It last, FwdIt dest)
 {
-    return uninitialized_copy(
-        scratch::move_iterator(first),
-        scratch::move_iterator(last),
-        dest
-    ).base();
+    using T = iterator_value_type_t<FwdIt>;
+    scratch::allocator<T> a;
+    return uninitialized_move(first, last, dest, a);
 }
 
 template<class It, class FwdIt>
 FwdIt uninitialized_move_if_noexcept(It first, It last, FwdIt dest)
 {
-    if constexpr (is_constructible_v<iterator_value_type_t<FwdIt>, const iterator_value_type_t<It>&> &&
-                  !is_nothrow_constructible_v<iterator_value_type_t<FwdIt>, iterator_value_type_t<It>&&>) {
-        return uninitialized_copy(first, last, dest);
-    } else {
-        return uninitialized_move(first, last, dest);
-    }
+    using T = iterator_value_type_t<FwdIt>;
+    scratch::allocator<T> a;
+    return uninitialized_move_if_noexcept(first, last, dest, a);
 }
 
 template<class It, class Size, class FwdIt>
 FwdIt uninitialized_copy_n(It first, Size count, FwdIt dest)
 {
     using T = iterator_value_type_t<FwdIt>;
-    return detail::uninitialized_construct_n_via(dest, count, [&](T *p) {
-        ::new (p) T(*first);
-        ++first;
-    });
+    scratch::allocator<T> a;
+    return uninitialized_copy_n(first, count, dest, a);
 }
 
 template<class It, class Size, class FwdIt>
 FwdIt uninitialized_move_n(It first, Size count, FwdIt dest)
 {
-    return uninitialized_copy_n(
-        scratch::move_iterator(first),
-        count,
-        dest
-    ).base();
+    using T = iterator_value_type_t<FwdIt>;
+    scratch::allocator<T> a;
+    return uninitialized_move_n(first, count, dest, a);
 }
 
 template<class It, class Size, class FwdIt>
 FwdIt uninitialized_move_if_noexcept_n(It first, Size count, FwdIt dest)
 {
-    if constexpr (is_constructible_v<iterator_value_type_t<FwdIt>, const iterator_value_type_t<It>&> &&
-                  !is_nothrow_constructible_v<iterator_value_type_t<FwdIt>, iterator_value_type_t<It>&&>) {
-        return uninitialized_copy_n(first, count, dest);
-    } else {
-        return uninitialized_move_n(first, count, dest);
-    }
+    using T = iterator_value_type_t<FwdIt>;
+    scratch::allocator<T> a;
+    return uninitialized_move_if_noexcept_n(first, count, dest, a);
 }
 
-template<class FwdIt, class T>
-void uninitialized_fill(FwdIt dest, FwdIt last, const T& value)
+template<class FwdIt, class U>
+void uninitialized_fill(FwdIt dest, FwdIt last, const U& value)
 {
     using T = iterator_value_type_t<FwdIt>;
-    detail::uninitialized_construct_via(dest, last, [&](T *p) {
-        ::new (p) T(value);
-    });
+    scratch::allocator<T> a;
+    return uninitialized_fill(dest, last, value, a);
 }
 
-template<class FwdIt, class Size, class T>
-FwdIt uninitialized_fill_n(FwdIt dest, Size count, const T& value)
+template<class FwdIt, class Size, class U>
+FwdIt uninitialized_fill_n(FwdIt dest, Size count, const U& value)
 {
     using T = iterator_value_type_t<FwdIt>;
-    return detail::uninitialized_construct_n_via(dest, count, [&](T *p) {
-        ::new (p) T(value);
-    });
+    scratch::allocator<T> a;
+    return uninitialized_fill_n(dest, count, value, a);
 }
 
 template<class FwdIt>
 void uninitialized_default_construct(FwdIt dest, FwdIt last)
 {
     using T = iterator_value_type_t<FwdIt>;
-    detail::uninitialized_construct_via(dest, last, [](T *p) {
-        ::new (p) T;
-    });
+    scratch::allocator<T> a;
+    uninitialized_default_construct(dest, last, a);
 }
 
 template<class FwdIt, class Size>
 FwdIt uninitialized_default_construct_n(FwdIt dest, Size count)
 {
     using T = iterator_value_type_t<FwdIt>;
-    return detail::uninitialized_construct_n_via(dest, count, [](T *p) {
-        ::new (p) T;
-    });
+    scratch::allocator<T> a;
+    uninitialized_default_construct_n(dest, count, a);
 }
 
 template<class FwdIt>
 void uninitialized_value_construct(FwdIt dest, FwdIt last)
 {
     using T = iterator_value_type_t<FwdIt>;
-    detail::uninitialized_construct_via(dest, last, [](T *p) {
-        ::new (p) T();
-    });
+    scratch::allocator<T> a;
+    uninitialized_value_construct(dest, last, a);
 }
 
 template<class FwdIt, class Size>
 void uninitialized_value_construct_n(FwdIt dest, Size count)
 {
     using T = iterator_value_type_t<FwdIt>;
-    return detail::uninitialized_construct_n_via(dest, count, [](T *p) {
-        ::new (p) T();
-    });
+    scratch::allocator<T> a;
+    uninitialized_value_construct_n(dest, count, a);
 }
 
 } // namespace scratch
