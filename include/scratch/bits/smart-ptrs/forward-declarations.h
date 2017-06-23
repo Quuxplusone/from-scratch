1762b6be9e26523e468cfd6bca0a4c4ac9085327
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-06-23 11:02:44 -0700

    Implement `weak_ptr<T>`.
    This includes an implementation of `shared_ptr.unlock()` from my paper
    N4537 "Adding symmetry between shared_ptr and weak_ptr".

--- include/scratch/bits/smart-ptrs/forward-declarations.h
+++ include/scratch/bits/smart-ptrs/forward-declarations.h
@@ -6,4 +6,7 @@ namespace scratch {
 
 template<typename T, typename D = default_delete<T>> class unique_ptr;  // forward declaration
 
+template<typename T> class shared_ptr;  // forward declaration
+template<typename T> class weak_ptr;  // forward declaration
+
 } // namespace scratch
