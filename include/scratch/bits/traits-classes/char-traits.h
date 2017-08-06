0c0f22490414b69690f23cc66248bc39440c1664
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-08-06 12:14:34 -0700

    Implement `char_traits`-awareness for `basic_string`.
    This means using `Traits::assign` whenever a character is coming into the
    string from "outside"; and using `Traits::length` to get the length of
    a string from "outside". The standard `char_traits` also provides
    `Traits::copy` and `Traits::move`, but it's not clear why we'd want to
    use them instead of `std::copy` and `std::move`.
    
    Both libc++ and libstdc++ use these trait functions:
    
        assign, compare, copy, eq, find, length, move
    
    libc++ requires that `Traits::eq` not be overloaded.
    
    The standard `char_traits<T>::copy()` returns a copy of its first argument,
    i.e., it simulates `memcpy` rather than `std::copy`. However, since the caller
    cannot possibly have a use for this value (the caller's already got a copy of
    it), I've simplified my library's version by just returning `void`.

--- include/scratch/bits/traits-classes/char-traits.h
+++ include/scratch/bits/traits-classes/char-traits.h
@@ -1,12 +1,37 @@
 #pragma once
 
+#include <cstddef>
+
 namespace scratch {
 
 template<class> struct string_related_char_traits;
 template<class> struct iostream_related_char_traits;
 
 template<>
-struct string_related_char_traits<char> {
+struct string_related_char_traits<char>
+{
+    static constexpr size_t length(const char *s) {
+        for (size_t n = 0; true; ++s, ++n) {
+            if (*s == '\0') return n;
+        }
+    }
+    static constexpr void assign(char& r, const char& a) {
+        r = a;
+    }
+#if 0
+    static void assign(char *p, size_t count, char a) {
+        while (count) {
+            *p++ = a;
+            --count;
+        }
+    }
+#endif
+    static void copy(char *dst, const char *src, size_t count) {
+        while (count) {
+            *dst++ = *src++;
+            --count;
+        }
+    }
 };
 
 template<>
@@ -14,7 +39,7 @@ struct iostream_related_char_traits<char> {
 };
 
 template<class T>
-struct char_traits<T> : string_related_char_traits<T>, iostream_related_char_traits<T> {
+struct char_traits : string_related_char_traits<T>, iostream_related_char_traits<T> {
     using char_type = T;
 };
 
